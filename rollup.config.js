ab248da3ffafb9731d0f323033ac80dbca6481cf
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-07-09 12:44:29 +0200

    configure rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
 import { defineConfig } from 'rollup';
 
 export default defineConfig({
@@ -18,9 +19,14 @@ export default defineConfig({
     },
   ],
   plugins: [
+    url({ include: ['**/*.wav'], limit: 1000000 }), // Inline if < 1MB, otherwise emit as file
     nodeResolve(),
     commonjs(),
-    typescript({ tsconfig: './tsconfig.json' }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
   ],
   external: ['react', 'react-dom', 'framer-motion'],
 });
