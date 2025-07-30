eaa8be4a994e254d303039142cdf54ca2d73093f
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-07-31 00:54:27 +0200

    chore: clean up generated files and build artifacts
    - Remove .rollup.cache directory and generated build files
    - Clean up TypeScript build info files
    - Remove package-lock.json (using bun.lock instead)
    - Ensure clean repository state for development

--- .rollup.cache/home/goitseone/Projects/web/log-it-raw/raw3d-ui/src/components/List3D.js
+++
@@ -1,9 +0,0 @@
-// src/components/List3D.tsx
-import styled from 'styled-components';
-const List3D = styled.ul `
-  perspective: 1000px;
-  list-style: none;
-  padding: 0;
-`;
-export { List3D };
-//# sourceMappingURL=List3D.js.map
